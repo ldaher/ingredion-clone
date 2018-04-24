0588cfdf7a37d7fdd9cd2f689a9e792974768f0e
luciano.daher <luciano.daher@BRZBEL484L.ad.infosys.com>
2018-04-24 15:13:04 -0300

    retornando versão Java para 1.5 e commons-net para 1.4.1

--- src/main/java/training/java/apachecommon/net/TelnetClientApp.java
+++ src/main/java/training/java/apachecommon/net/TelnetClientApp.java
@@ -8,10 +8,10 @@ import training.java.apachecommon.net.bean.BeanTelnetClient;
 public class TelnetClientApp {
 
 	public static void main(String[] args) {
-
 		ApplicationContext context = new ClassPathXmlApplicationContext("context-spring.xml");
 
 		BeanTelnetClient bean = (BeanTelnetClient) context.getBean("beanTelnetClient");
+		
 
 		System.out.println(
 				"Para finalizar a aplicação basta digitar \"exit\" ou \"quit\" em qualquer prompt...");
